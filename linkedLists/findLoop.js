f0ad1a343cfc93a09541bac5ec844e27002c4b3c
Anshuj <anshuj@gmail.com>
2019-12-22 08:30:05 -0800

    Find loop - added comments

--- linkedLists/findLoop.js
+++ linkedLists/findLoop.js
@@ -1,24 +1,25 @@
 class LinkedList {
-    constructor(value) {
-        this.value = value;
-        this.next = null;
-    }
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
 function findLoop(head) {
-    // Write your code here.
-    let fastPtr = head.next.next;
-    let slowPtr = head.next;
+  // Write your code here.
+  let fastPtr = head.next.next;
+  let slowPtr = head.next;
 
-    while (fastPtr !== slowPtr) {
-        fastPtr = fastPtr.next.next;
-        slowPtr = slowPtr.next;
-    }
-    fastPtr = head;
-    while (fastPtr !== slowPtr) {
-        fastPtr = fastPtr.next
-        slowPtr = slowPtr.next;
-    }
-    return fastPtr;
+  while (fastPtr !== slowPtr) {
+    fastPtr = fastPtr.next.next;
+    slowPtr = slowPtr.next;
+  }
+  fastPtr = head;
+  //Based on the explanation by algoexperts
+  //Now both pointers move at the same pace
+  while (fastPtr !== slowPtr) {
+    fastPtr = fastPtr.next;
+    slowPtr = slowPtr.next;
+  }
+  return fastPtr;
 }
-
